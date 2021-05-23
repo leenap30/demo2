cb23167c8cee6e8f9601bda73d05bfb260f3d9d7
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-23 19:28:47 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,10 +1,5 @@
-AWS Config
-variable "aws_access_key" {
-default = "AKIAWBVGC7S5KOEQCZ6L"
-}
-variable "aws_secret_key" {
-default = "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"
-}
+AWS Config{
 variable "aws_region" {
 default = "us-east-2"
+}
 }
