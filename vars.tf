527c0edd27b33954b201b361f42c857639ca0cb9
leenap30 <82202830+leenap30@users.noreply.github.com>
2021-05-23 19:18:57 +0530

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,10 @@
+AWS Config
+variable "aws_access_key" {
+default = "PASTE_ACCESS_KEY_HERE"
+}
+variable "aws_secret_key" {
+default = "PASTE_SECRET_KEY_HERE"
+}
+variable "aws_region" {
+default = "ENTER_AWS_REGION"
+}
